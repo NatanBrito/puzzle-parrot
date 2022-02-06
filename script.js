6f5504359f40f92d5ec680d2d081fa5416d02bc8
Natan Brito_3 <natanismaelbrito@gmail.com>
2022-02-06 03:58:17 -0300

    consegui fazer ficar atrás da carta, mas não some o papagaio

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
 let numCards;
 let makerCards;
-let versoImg=
-["<img class='gif disappears' src='imagens/bobrossparrot.gif' alt='papagaio de chapéu'",
-"<img class='gif disappears' src='imagens/explodyparrot.gif' alt='papagaio indo pro lado'",
-"<img class='gif disappears' src='imagens/fiestaparrot.gif' alt='papagaio com chapéu mexicano'",
-"<img class='gif disappears' src='imagens/metalparrot.gif' alt='papagaio do rock'",
-"<img class='gif disappears' src='imagens/revertitparrot.gif' alt='papagaio navegando'",
-"<img class='gif disappears' src='imagens/tripletsparrot.gif' alt=' 3 papagaio dançando'",
-"<img class='gif disappears' src='imagens/unicornparrot.gif' alt='papagaio unicórnio'",
-];
+let versoImg =
+  ["<img class='gif' src='imagens/bobrossparrot.gif' alt='papagaio de chapéu'",
+    "<img class='gif ' src='imagens/explodyparrot.gif' alt='papagaio indo pro lado'",
+    "<img class='gif ' src='imagens/fiestaparrot.gif' alt='papagaio com chapéu mexicano'",
+    "<img class='gif ' src='imagens/metalparrot.gif' alt='papagaio do rock'",
+    "<img class='gif ' src='imagens/revertitparrot.gif' alt='papagaio navegando'",
+    "<img class='gif ' src='imagens/tripletsparrot.gif' alt=' 3 papagaio dançando'",
+    "<img class='gif ' src='imagens/unicornparrot.gif' alt='papagaio unicórnio'",
+  ];
 //embaralha as cartas
 versoImg.sort(comparador);
-function comparador() { 
-	return Math.random() - 0.5;
+function comparador() {
+  return Math.random() - 0.5;
 }
 
-  while (numCards % 2 != 0 || numCards > 14 || numCards < 4) {
-    numCards = parseInt(prompt("Qual o número de cartas? Somente número par entre 4 a 14"));
+while (numCards % 2 != 0 || numCards > 14 || numCards < 4) {
+  numCards = parseInt(prompt("Qual o número de cartas? Somente número par entre 4 a 14"));
 }
 // pra gerar a carta
-makerCards= document.querySelector("nav")
-for(let i=0; i<numCards;i++){
-  const frontCardHtml=`   <div class="card " onclick=" turnCard(this);" > 
-  <img src="./imagens/parrot.png"/>
-  
+makerCards = document.querySelector("nav")
+for (let i = 0; i < numCards; i++) {
+  const frontCardHtml = `   <div class="card " onclick=" turnCard(this);" > 
+  <img class=frontFace src="./imagens/parrot.png"/>
+   <div class="backFace">
+   ${versoImg[i]}
+  </div>  
 </div>`
-makerCards.innerHTML += frontCardHtml
+  makerCards.innerHTML += frontCardHtml
 }
-// tentando fazer funcionar, girando só a primeira,
-function turnCard(chamandoCarta){
-const test = document.querySelector(".card")
-test.classList.toggle("turnCard")
+
+const test = document.querySelectorAll(".card")
+test.forEach(card => card.addEventListener('click', turnCard))
+
+function turnCard() {
+  this.classList.add("turnCard")
 }
 
