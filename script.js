7cf9a165b8ddcb2cf0cdd434ac2b1703322f17cb
Natan Brito_3 <natanismaelbrito@gmail.com>
2022-02-07 12:35:22 -0300

    win and fail funcionando, falta ajustes, contador de jogadas,prompt da vitória e ajustes

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
+let thisAntes = [];
+let thisAtual = [];
+let primeiraCarta ;
+let segundaCarta  ;
 let numCards;
-let cartasCriadas=[]
-let texteFor;
+let thisBefore = []
+let cartasCriadas = []
+let firstCard;
+let secondCard;
 let makerCards;
 let versoImg =
-  ["<img class='gif' src='imagens/bobrossparrot.gif' alt='papagaio de chapéu'",
-    "<img class='gif ' src='imagens/explodyparrot.gif' alt='papagaio indo pro lado'",
-    "<img class='gif ' src='imagens/fiestaparrot.gif' alt='papagaio com chapéu mexicano'",
-    "<img class='gif ' src='imagens/metalparrot.gif' alt='papagaio do rock'",
-    "<img class='gif ' src='imagens/revertitparrot.gif' alt='papagaio navegando'",
-    "<img class='gif ' src='imagens/tripletsparrot.gif' alt=' 3 papagaio dançando'",
-    "<img class='gif ' src='imagens/unicornparrot.gif' alt='papagaio unicórnio'",
+  ["<img class='gif' src='imagens/bobrossparrot.gif'",
+    "<img class='gif ' src='imagens/explodyparrot.gif'",
+    "<img class='gif ' src='imagens/fiestaparrot.gif'",
+    "<img class='gif ' src='imagens/metalparrot.gif'",
+    "<img class='gif ' src='imagens/revertitparrot.gif'",
+    "<img class='gif ' src='imagens/tripletsparrot.gif'",
+    "<img class='gif ' src='imagens/unicornparrot.gif'",
   ];
 //embaralha as cartas
 versoImg.sort(comparador);
@@ -22,25 +28,59 @@ while (numCards % 2 != 0 || numCards > 14 || numCards < 4) {
 }
 // pra gerar a carta
 makerCards = document.querySelector("nav")
-for (let i = 0; i < (numCards/2); i++) {
-  const frontCardHtml = `   <div data-identifier="card" class="card " onclick=" turnCard(this);" > 
+for (let i = 0; i < (numCards / 2); i++) {
+  const frontCardHtml = `   <div data-identifier="card" class="card "  onclick=" turnCard(this);" > 
    
   <img data-identifier="back-face" class="backFace" src="./imagens/parrot.png"/>
   <div data-identifier="front-face" class="frontFace">
   ${versoImg[i]}
  </div>  
 </div>`
-cartasCriadas.push(frontCardHtml)
-cartasCriadas.push(frontCardHtml)
-cartasCriadas.sort(comparador);
+  cartasCriadas.push(frontCardHtml)
+  cartasCriadas.push(frontCardHtml)
+  cartasCriadas.sort(comparador);
 }
-for(let i=0;i<cartasCriadas.length;i++){
-makerCards.innerHTML += cartasCriadas[i];
+for (let i = 0; i < cartasCriadas.length; i++) {
+  makerCards.innerHTML += cartasCriadas[i];
 }
 const test = document.querySelectorAll(".card")
 test.forEach(card => card.addEventListener('click', turnCard))
-
+// faz a carta virar e tentando fazer o confirmador de cartas iguais
 function turnCard() {
-  this.classList.add("turnCard")
+this.classList.add("turnCard")
+
 }
 
+function turnCard(diviSelecionada){
+  diviSelecionada.classList.add('turnCard');
+  if(!document.querySelector('.primeiraCarta')){
+      diviSelecionada.classList.add('primeiraCarta');
+      primeiraCarta = diviSelecionada;
+      primeiraCarta.setAttribute('onclick','')
+      console.log(primeiraCarta);
+      return false;
+  }
+      diviSelecionada.classList.add('segundaCarta');
+      segundaCarta = diviSelecionada;
+      console.log(segundaCarta);
+      setTimeout(compararCartas,1000);
+      console.log(document.querySelectorAll('.estouComMeuPar').length);
+      console.log(asCartas.length);
+}
+function compararCartas(){
+  if(primeiraCarta.innerHTML !== segundaCarta.innerHTML){
+      primeiraCarta.classList.remove('primeiraCarta');
+      primeiraCarta.classList.remove('turnCard');
+      segundaCarta.classList.remove('segundaCarta');
+      segundaCarta.classList.remove('turnCard');
+      primeiraCarta.setAttribute('onclick','virarCarta(this)');
+  }
+  else{
+      primeiraCarta.classList.remove('primeiraCarta');
+      primeiraCarta.classList.add('estouComMeuPar');
+      primeiraCarta.setAttribute('onclick','');
+      segundaCarta.classList.remove('segundaCarta');
+      segundaCarta.setAttribute('onclick','');
+      segundaCarta.classList.add('estouComMeuPar');
+  }
+} 
